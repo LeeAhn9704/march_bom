0221bd33f4fc302289b008dec7bf2f1280cd647a
LeeAhn9704 <102342865+LeeAhn9704@users.noreply.github.com>
2022-05-23 16:33:53 +0900

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,6 +1,6 @@
 <?php
-	$db_con = mysqli_connect("localhost", "bnm3614", "비밀번호");
-	$db_select = mysqli_select_db($db_con,"bnm3614");
+	$db_con = mysqli_connect("localhost", "아이디", "비밀번호");
+	$db_select = mysqli_select_db($db_con,"아이디");
 	mysqli_query($db_con,"set names utf8");
 
 	function db_close()
